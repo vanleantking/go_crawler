6c2a3f393f2608e94047f13e02db7079228dab78
vanleantking <van.le.k3c1@gmail.com>
2019-02-23 19:52:43 +0700

    optimize mongodb connection

--- utils/mongo_driver.go
+++ utils/mongo_driver.go
@@ -14,77 +14,9 @@ type ClientMGO struct {
 	CancelFunc context.CancelFunc
 }
 
-func ConnectMGOHistoryDB() (error, *ClientMGO) {
-	link := "mongodb://" + USERNAME1 + ":" + PASSWORD1 + "@" + HISTORY_ONLY + ":" + PORT1 + "/" + DATABASE1
-	mgoclient := &ClientMGO{}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	mgoclient.Ctx = ctx
-	mgoclient.CancelFunc = cancel
-	client, err := mongo.Connect(mgoclient.Ctx, link, nil)
-	mgoclient.Client = client
-	if err != nil {
-		fmt.Println(err, client)
-		return err, mgoclient
-	}
-
-	return nil, mgoclient
-}
-
-func ConnectMGODMPDB() (error, *ClientMGO) {
-
-	link := "mongodb://" + USERNAME3 + ":" + PASSWORD3 + "@" + MY_HOST + ":" + PORT1 + "/" + DATABASE3
-	mgoclient := &ClientMGO{}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	mgoclient.Ctx = ctx
-	mgoclient.CancelFunc = cancel
-	client, err := mongo.Connect(mgoclient.Ctx, link, nil)
-	mgoclient.Client = client
-	if err != nil {
-		fmt.Println(err, client)
-		return err, mgoclient
-	}
-
-	return nil, mgoclient
-}
-
-func ConnectMGOIPInfo() (error, *ClientMGO) {
-	link := "mongodb://" + USERNAME5 + ":" + PASSWORD5 + "@" + IPINFO + ":" + PORT4 + "/" + DATABASE3
-	mgoclient := &ClientMGO{}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	mgoclient.Ctx = ctx
-	mgoclient.CancelFunc = cancel
-	client, err := mongo.Connect(mgoclient.Ctx, link, nil)
-	mgoclient.Client = client
-	if err != nil {
-		fmt.Println(err, client)
-		return err, mgoclient
-	}
-
-	return nil, mgoclient
-}
-
-func ConnectMGODataOnlyDB() (error, *ClientMGO) {
-	link := "mongodb://" + USERNAME2 + ":" + PASSWORD2 + "@" + ADDRESS + ":" + PORT2 + "/" + DATABASE2
-	mgoclient := &ClientMGO{}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	mgoclient.Ctx = ctx
-	mgoclient.CancelFunc = cancel
-	client, err := mongo.Connect(mgoclient.Ctx, link, nil)
-	mgoclient.Client = client
-	if err != nil {
-		fmt.Println(err, client)
-		return err, mgoclient
-	}
-
-	return nil, mgoclient
-}
-
-func ConnectMGODMPLog() (error, *ClientMGO) {
-	link := "mongodb://" + USERNAME4 + ":" + PASSWORD4 + "@" + DMP_LOG + ":" + PORT3 + "/" + DATABASE4
+func ConnectMongoDB(database string) (error, *ClientMGO) {
+	dbInfo := DBCONFIG[database]
+	link := "mongodb://" + dbInfo.Username + ":" + dbInfo.Password + "@" + dbInfo.Address + ":" + dbInfo.Port + "/" + dbInfo.Database
 	mgoclient := &ClientMGO{}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
