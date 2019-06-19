314a44d9af895d6a2d75060c68f6196251e5634f
vanleantking <van.le.k3c1@gmail.com>
2019-06-19 13:37:20 +0700

    update proxy status crawl

--- crwl_social/crw_oto.go
+++ crwl_social/crw_oto.go
@@ -213,17 +213,20 @@ func crawlURL(wg *sync.WaitGroup) {
 						proxy_str := strings.Replace(er.Error(), settings.ErrProxyPrefix, "", -1)
 						re := regexp.MustCompile(RegexpProxy)
 
-						proxy_pieces := re.FindAllString(strings.TrimSpace(proxy_str), -1)
-						ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
-						_, er = proxy_collection.UpdateOne(
-							ctx,
-							bson.M{
-								"proxy_ip": strings.TrimSpace(proxy_pieces[1]),
-								"port":     strings.TrimSpace(proxy_pieces[2]),
-								"schema":   strings.TrimSpace(proxy_pieces[0])},
-							bson.M{"$set": bson.M{"status": false}})
-						if er != nil {
-							log.Println("Can not update status proxy, ", er.Error())
+						if re.MatchString(proxy_str) {
+							proxy_pieces := re.FindAllString(strings.TrimSpace(proxy_str), -1)
+							log.Println("Proxy error, ", proxy_pieces, strings.TrimSpace(proxy_str), er.Error())
+							ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
+							_, er = proxy_collection.UpdateOne(
+								ctx,
+								bson.M{
+									"proxy_ip": strings.TrimSpace(proxy_pieces[1]),
+									"port":     strings.TrimSpace(proxy_pieces[2]),
+									"schema":   strings.TrimSpace(proxy_pieces[0])},
+								bson.M{"$set": bson.M{"status": false}})
+							if er != nil {
+								log.Println("Can not update status proxy, ", er.Error())
+							}
 						}
 						continue
 					} else {
